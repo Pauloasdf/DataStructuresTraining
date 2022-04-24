92a31b7f0482ed98a53132ea6bb1459179368471
Pauloasdf <paulopiresavila@gmail.com>
2022-04-24 15:51:47 -0300

    Included MaxSubArray problem solution

---
+++ DataStructuresTraining/Extensions/ArrayExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresTraining.Extensions
+{
+    public static class ArrayExtensions
+    {
+		public static int MaxSubArray(this int[] nums)
+		{
+			int currentSum = nums[0];
+			int maximumSum = nums[0];
+
+			for (int i = 1; i < nums.Length; i++)
+			{
+				currentSum = Math.Max(currentSum + nums[i], nums[i]);
+				maximumSum = Math.Max(maximumSum, currentSum);
+			}
+
+			return maximumSum;
+		}
+	}
+}
