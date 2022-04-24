015715469201389cbc97695fe982d3942a244e18
Pauloasdf <paulopiresavila@gmail.com>
2022-04-24 16:24:28 -0300

    Added solution for PlusOne and removed Prints from Helper to Extension methods

--- DataStructuresTraining/Extensions/ArrayExtensions.cs
+++ DataStructuresTraining/Extensions/ArrayExtensions.cs
@@ -21,5 +21,33 @@ namespace DataStructuresTraining.Extensions
 
 			return maximumSum;
 		}
-	}
+		public static void Print(this int[] array)
+		{
+			for(int i = 0; i < array.Length; i++)
+            {
+				Console.Write($"{array[i]} ");
+            }
+			Console.Write("\n");
+		}
+        public static int[] PlusOne(this int[] digits)
+        {
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                if (i == 0 && digits[i] == 9)
+                {
+                    digits[i] = 0;
+                    return digits.Prepend(1).ToArray();
+                }
+
+                if (digits[i] != 9)
+                {
+                    digits[i] = digits[i] + 1;
+                    return digits;
+                }
+                else
+                    digits[i] = 0;
+            }
+            return digits;
+        }
+    }
 }
---
+++ DataStructuresTraining/Extensions/ListExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresTraining.Extensions
+{
+    public static class ListExtensions
+    {
+        public static void PrintListOfInt(this List<int> list)
+        {
+            list.ForEach(x => Console.WriteLine($"{x} "));
+        }
+    }
+}
