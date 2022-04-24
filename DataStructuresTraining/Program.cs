a87932d1c99f51b5dbdb0691625832e1f95617c9
Pauloasdf <paulopiresavila@gmail.com>
2022-04-24 17:15:02 -0300

    Partial save on binary sum problem

--- DataStructuresTraining/Program.cs
+++ DataStructuresTraining/Program.cs
@@ -2,8 +2,35 @@
 using DataStructuresTraining.Extensions;
 using static DataStructuresTraining.Models.Enums;
 
-IntHeap heap = new(20, HeapType.MinHeap);
-for(int i = 0; i < heap.Elements.Length; i++)
+Console.WriteLine(AddBinary("11", "1"));
+Console.WriteLine(AddBinary("1010", "1011"));
+
+string AddBinary(string firstBinaryNumber, string SecondBinaryNumber)
 {
-    heap.InsertElement(new Random().Next(heap.Elements.Length));
+    string result = "";
+    char lastColumnSumRemaining = '0';
+
+    for (int i = 1; i <= firstBinaryNumber.Length; i++)
+    {
+        if (firstBinaryNumber[^i] == '1')
+        {
+            (result, lastColumnSumRemaining) = CalculateColumnSum(SecondBinaryNumber, result, (i <= SecondBinaryNumber.Length 
+                                                                                    ? SecondBinaryNumber[^i] 
+                                                                                    : lastColumnSumRemaining));
+
+            if(i == firstBinaryNumber.Length && result[0] == '0') 
+                result = $"1{result}";
+        }
+
+
+    }
+
+    return result;
+}
+
+(string, char) CalculateColumnSum(string b, string result, char curPosition)
+{
+    char curSumResult = curPosition == '1' ? '0' : '1';
+    char curSumRemain = curSumResult == '1' ? '0' : '1';
+    return ($"{curSumResult}{result}", curSumRemain);
 }
